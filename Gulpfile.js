670fb0ba6fb335c540cd86c907f3ab5bfe905ea6
makotot <makoto.ttn@gmail.com>
2014-07-20 23:28:15 +0900

    + lib

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
+  bower = require('bower'),
   eslint = require('gulp-eslint'),
-  assemble = require('gulp-assemble');
+  assemble = require('gulp-assemble'),
+  connect = require('gulp-connect'),
+  less = require('gulp-less'),
+  path = require('path'),
+  rimraf = require('rimraf'),
+  runSequence = require('run-sequence');
 
 var option = {
   assemble: {
@@ -9,17 +15,75 @@ var option = {
   }
 };
 
+
+gulp.task('rm', function (cb) {
+  rimraf('./_gh_pages', cb);
+});
+
+
+gulp.task('setup', function () {
+  bower.commands.install().on('end', function () {
+    gulp.src([
+      './bower_components/jquery/dist/jquery.min.js'
+    ])
+    .pipe(gulp.dest('_gh_pages/js/lib'));
+
+    gulp.src([
+      './bower_components/normalize-css/normalize.css'
+    ])
+    .pipe(gulp.dest('_gh_pages/css/lib'));
+  });
+});
+
+
+gulp.task('connect', function () {
+  connect.server({
+    root: '_gh_pages',
+    livereload: true
+  });
+});
+
 gulp.task('lint', function () {
-  gulp.src(['./*.js'])
+  return gulp.src(['./*.js'])
     .pipe(eslint())
-    .pipe(eslint.format());
+    .pipe(eslint.format())
+    .pipe(connect.reload());
 });
 
 gulp.task('assemble', function () {
-  gulp.src('src/pages/*.hbs')
+  return gulp.src('src/pages/*.hbs')
     .pipe(assemble(option.assemble))
-    .pipe(gulp.dest('_gh_pages'));
+    .pipe(gulp.dest('_gh_pages'))
+    .pipe(connect.reload());
+});
+
+gulp.task('less', function () {
+  return gulp.src('src/less/**/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(gulp.dest('_gh_pages/css/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('compile', function (cb) {
+  runSequence(
+    'rm',
+    'setup',
+    'assemble',
+    'less',
+    cb
+  );
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/**/*.hbs'], ['assemble']);
+  gulp.watch(['./*.js'], ['lint']);
+  gulp.watch(['./src/less/**/*.less'], ['less']);
 });
 
 
-gulp.task('default', ['lint', 'assemble']);
+gulp.task('default', ['lint', 'compile']);
+gulp.task('ci', ['lint', 'compile']);
+gulp.task('serve', ['compile', 'connect', 'watch']);
+
