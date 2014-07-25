eb31852447da41ab58bd9254e8d0ca50e66cfb73
makotot <makoto.ttn@gmail.com>
2014-07-25 23:09:20 +0900

    + ls

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@
 
 cd _gh-pages/
 git init
-git config user.name "makotot"
-git config user.email "makoto.ttn@gmail.com"
-git add .
-git commit -m "Deploy to gh-pages"
-git push -fq "https://${GH_TOKEN}@github.com/${GH_REPO}.git" master:gh-pages > /dev/null 2>&1
+git config user.name "${GIT_COMITTER_NAME}"
+git config user.email "${GIT_COMITTER_EMAIL}"
+ls -la
+#git add .
+#git commit -m "Deploy to gh-pages"
+#git push -fq "https://${GH_TOKEN}@github.com/${GH_REPO}.git" master:gh-pages > /dev/null 2>&1
 
